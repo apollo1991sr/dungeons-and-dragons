ff0429f59a82e3462467975098977814c612e73f
apollo1991sr <apollo1991sr@gmail.com>
2026-09-18 10:05:57 +0000

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,28 @@
         @yield('title', 'Dungeons & Dragons')
     </title>
 
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+
+    <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="{{ asset('favicon-32x32.png') }}"
+    >
+
+    <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="{{ asset('favicon-16x16.png') }}"
+    >
+
+    <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="{{ asset('apple-touch-icon.png') }}"
+    >
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
